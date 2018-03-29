7325fe9a256296b221bbba5543e8c51b8b9c4a45
PS001 <luigi.zielone@gmail.com>
2018-03-29 23:41:50 +0200

    onp calculator - floating numbers

--- lab1/onp.erl
+++ lab1/onp.erl
@@ -10,18 +10,25 @@
 -author("Ludwik Ciechański").
 
 %% API
--export([onp/1, calculate/2]).
+-export([onp/1, calculate/2, strnum/1]).
+
+%% to_float(String) -> {Float, Rest} | {error, Reason}
+strnum(Input) ->
+  case string:to_float(Input) of
+    {error, no_float} -> list_to_integer(Input);
+    {Float, _Rest} -> Float
+  end.
 
 calculate(["+" | T], [A | [B | R]]) -> calculate(T, [B + A | R]);
+calculate(["-" | T], [A | [B | R]]) -> calculate(T, [B - A | R]);
 calculate(["*" | T], [A | [B | R]]) -> calculate(T, [B * A | R]);
 calculate(["/" | T], [A | [B | R]]) -> calculate(T, [B / A | R]);
-calculate(["-" | T], [A | [B | R]]) -> calculate(T, [B - A | R]);
 calculate(["p" | T], [A | [B | R]]) -> calculate(T, [math:pow(B,A) | R]);
 calculate(["q" | T], [A | R])       -> calculate(T, [math:sqrt(A) | R]);
 calculate(["c" | T], [A | R])       -> calculate(T, [math:cos(A) | R]);
 calculate(["s" | T], [A | R])       -> calculate(T, [math:sin(A) | R]);
 calculate(["t" | T], [A | R])       -> calculate(T, [math:tan(A) | R]);
-calculate([H | T], S)               -> calculate(T, [list_to_integer(H)] ++ S);
+calculate([H | T], S)               -> calculate(T, [strnum(H)] ++ S);
 calculate([], [Result])             -> Result.
 
-onp(Expr) -> calculate(string:tokens(Expr," "),[]).
+onp(Input) -> calculate(string:tokens(Input," "),[]).
