eb40397a2d95eed0e5d2557ae21b81c3a66f7667
PS001 <luigi.zielone@gmail.com>
2018-03-29 20:01:18 +0200

    onp calculator - sqrt, pow, trygo

--- lab1/onp.erl
+++ lab1/onp.erl
@@ -12,11 +12,16 @@
 %% API
 -export([onp/1, calculate/2]).
 
-calculate(["+" | T], [A | [B | R]])   -> calculate(T, [B + A | R]);
-calculate(["*" | T], [A | [B | R]])   -> calculate(T, [B * A | R]);
-calculate(["/" | T], [A | [B | R]])   -> calculate(T, [B / A | R]);
-calculate(["-" | T], [A | [B | R]])   -> calculate(T, [B - A | R]);
-calculate([H | T], Stack)             -> calculate(T, [list_to_integer(H)] ++ Stack);
-calculate([], [Result])               -> Result.
+calculate(["+" | T], [A | [B | R]]) -> calculate(T, [B + A | R]);
+calculate(["*" | T], [A | [B | R]]) -> calculate(T, [B * A | R]);
+calculate(["/" | T], [A | [B | R]]) -> calculate(T, [B / A | R]);
+calculate(["-" | T], [A | [B | R]]) -> calculate(T, [B - A | R]);
+calculate(["p" | T], [A | [B | R]]) -> calculate(T, [math:pow(B,A) | R]);
+calculate(["q" | T], [A | R])       -> calculate(T, [math:sqrt(A) | R]);
+calculate(["c" | T], [A | R])       -> calculate(T, [math:cos(A) | R]);
+calculate(["s" | T], [A | R])       -> calculate(T, [math:sin(A) | R]);
+calculate(["t" | T], [A | R])       -> calculate(T, [math:tan(A) | R]);
+calculate([H | T], S)               -> calculate(T, [list_to_integer(H)] ++ S);
+calculate([], [Result])             -> Result.
 
 onp(Expr) -> calculate(string:tokens(Expr," "),[]).
