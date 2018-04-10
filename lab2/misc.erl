228a471c1a58445d5ecb65778cbbcd0a277f05dc
PS001 <luigi.zielone@gmail.com>
2018-04-10 18:23:50 +0200

    lab2 - misc: sumDigits and more

--- lab2/misc.erl
+++ lab2/misc.erl
@@ -10,8 +10,9 @@
 -author("Ludwik Ciechański").
 
 %% API
--export([randomElem/3]).
 -export([map/2, filter/2]).
+-export([sumDigits/1, splitNum/1]).
+-export([randomElem/3, sumDigDiv3/0]).
 
 randomElem(N, Min, Max) -> [rand:uniform(Max - Min + 1) + (Min - 1) || _ <- lists:seq(1, N)].
 
@@ -24,4 +25,10 @@ filter(Fun, List) -> [X || X <- List, Fun(X)].
 %% version 1
 %% sumDigits(N) -> lists:foldl(fun (X, Acc) -> X + Acc end, 0, lists:map(fun (X) -> X - $0 end, integer_to_list(N))).
 
+%% version 2
+splitNum(0) -> [];
+splitNum(N) -> splitNum(N div 10) ++ [N rem 10].
+sumDigits(N) -> lists:foldl(fun (X, Acc) -> X + Acc end, 0, splitNum(N)).
 
+%% filter 1M list, choose only numbers which sumDigits is divisible by 3.
+sumDigDiv3() -> lists:filter(fun (X) -> sumDigits(X) rem 3 == 0 end, randomElem(1000000, 1, 999999)).
