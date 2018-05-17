319815cd921e19ad75e4de3230b50deffacdbf2c
PS001 <luigi.zielone@gmail.com>
2018-05-17 17:21:39 +0200

    lab4: pollution_gen_server with supervisor

--- lab4/pollution_gen_server.erl
+++ lab4/pollution_gen_server.erl
@@ -13,7 +13,9 @@
 %% API
 -export([init/1, start_link/0, handle_call/3, handle_cast/2]).
 -export([start/0, stop/0, terminate/2, crash/0]).
--export([addStation/2]).
+-export([addStation/2, addValue/4, removeValue/3]).
+-export([getOneValue/3, getStationMean/2, getDailyMean/2]).
+-export([getMinimumDistanceStations/0]).
 %%%-------------------------------------------------------------------
 
 start_link() ->
@@ -31,16 +33,57 @@ start() -> start_link().
 stop() -> gen_server:cast(pgs, stop).
 %%%-------------------------------------------------------------------
 
-addStation(Name, Cords) -> gen_server:call(pgs, {addStation, {Name, Cords}}).
+addStation(Name, Cords) ->
+  gen_server:call(pgs, {addStation, {Name, Cords}}).
 
+addValue(Station, Date, Type, Value) ->
+  gen_server:call(pgs, {addValue, {Station, Date, Type, Value}}).
+
+removeValue(Station, Date, Type) ->
+  gen_server:call(pgs, {removeValue, {Station, Date, Type}}).
+
+getOneValue(Station, Date, Type) ->
+  gen_server:call(pgs, {getOneValue, {Station, Date, Type}}).
+
+getStationMean(Station, Type) ->
+  gen_server:call(pgs, {getStationMean, {Station, Type}}).
+
+getDailyMean(Type, Day) ->
+  gen_server:call(pgs, {getDailyMean, {Type, Day}}).
+
+getMinimumDistanceStations() ->
+  gen_server:call(pgs, {getMinimumDistanceStations}).
+%%%-------------------------------------------------------------------
 
 handle_call({addStation, {Name, Cords}}, _From, State) ->
   NewState = pollution:addStation(Name, Cords, State),
-  io:format("addStation(~p, ~p) ~n", [Name, Cords]),
-  {reply, {Name, Cords}, NewState}.
+  {reply, {Name, Cords}, NewState};
 
+handle_call({addValue, {Station, Date, Type, Value}}, _From, State) ->
+  NewState = pollution:addValue(Station, Date, Type, Value, State),
+  {reply, {Station, Date, Type, Value}, NewState};
 
+handle_call({removeValue, {Station, Date, Type}}, _From, State) ->
+  NewState = pollution:removeValue(Station, Date, Type, State),
+  {reply, {Station, Date, Type}, NewState};
+
+handle_call({getOneValue, {Station, Date, Type}}, _From, State) ->
+  OneValue = pollution:getOneValue(Station, Date, Type, State),
+  {reply, {OneValue}, State};
+
+handle_call({getStationMean, {Station, Type}}, _From, State) ->
+  StationMean = pollution:getStationMean(Station, Type, State),
+  {reply, {StationMean}, State};
+
+handle_call({getDailyMean, {Type, Day}}, _From, State) ->
+  DailyMean = pollution:getDailyMean(Type, Day, State),
+  {reply, {DailyMean}, State};
+
+handle_call({getMinimumDistanceStations}, _From, State) ->
+  MinDistStation = pollution:getMinimumDistanceStations(State),
+  {reply, {MinDistStation}, State}.
 %%%-------------------------------------------------------------------
+
 terminate(Reason, Value) ->
   io:format("server: exit ~p ~n", [Value]),
   Reason.
@@ -51,6 +94,6 @@ handle_cast(stop, Value) ->
   {stop, normal, Value};
 
 handle_cast(crash, State) ->
-  Illegal = 1 / 0,
+  1 / 0,
   {noreply, State}.
 %%%-------------------------------------------------------------------
--- lab4/pollution_server_supervisor.erl
+++ lab4/pollution_server_supervisor.erl
@@ -12,3 +12,20 @@
 %% API
 -export([start_link/1, init/1, start/0]).
 %%%-------------------------------------------------------------------
+
+start() -> start_link(empty).
+
+start_link(_) ->
+  supervisor:start_link(
+    {local, server_supervisor},
+    ?MODULE, []
+  ).
+
+init(_) ->
+  {ok, {
+    {one_for_all, 2, 3},
+    [ {pgs, {pollution_gen_server, start, []},
+      permanent, brutal_kill, worker,
+      [pollution_gen_server]}
+    ] }
+  }.
