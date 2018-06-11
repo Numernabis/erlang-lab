eeed911d2a9f264bba3fe03aaf503001a2177588
Numernabis <luigi.zielone@gmail.com>
2018-06-11 21:56:01 +0200

    lab5: identifyStations & timer

--- lab5/pollution_data.ex
+++ lab5/pollution_data.ex
@@ -2,14 +2,20 @@ defmodule PollutionData do
   @moduledoc false
 
   def importLinesFromCSV do
-    list = File.read!("pollution.csv") |> String.split()
+    list = File.read!("pollution.csv")
+           |> String.split()
     #length(list)
   end
 
   def parseOneLine(line) do
     [date, hour, longitude, latitude, value] = String.split(line, ",")
-    date = date |> String.split("-") |> Enum.reverse |> Enum.map(fn(x) -> elem(Integer.parse(x), 0) end) |> :erlang.list_to_tuple
-    hour = hour |> String.split(":") |> Enum.map(fn(x) -> elem(Integer.parse(x), 0) end) |> :erlang.list_to_tuple
+    date = date |> String.split("-")
+           |> Enum.reverse
+           |> Enum.map(fn(x) -> elem(Integer.parse(x), 0) end)
+           |> :erlang.list_to_tuple
+    hour = hour |> String.split(":")
+           |> Enum.map(fn(x) -> elem(Integer.parse(x), 0) end)
+           |> :erlang.list_to_tuple
     longitude = elem(Float.parse(longitude), 0)
     latitude = elem(Float.parse(latitude), 0)
     value = elem(Integer.parse(value), 0)
@@ -17,7 +23,8 @@ defmodule PollutionData do
   end
 
   def identifyStations do
-
+    map = parseLines() |> Enum.reduce(%{}, fn(d, acc) -> Map.put(acc, d.location, "xD") end)
+    #Map.size(map)
   end
 
   def addStations do
@@ -28,9 +35,12 @@ defmodule PollutionData do
 
   end
 
+  def parseLines do
+    importLinesFromCSV() |> Enum.map(fn(x) -> parseOneLine(x) end)
+  end
+
   def test do
-    [h | t] = importLinesFromCSV()
-    parseOneLine(h)
+    time_parsing = (fn() -> parseLines() end) |> :timer.tc |> elem(0)
   end
 
 end
