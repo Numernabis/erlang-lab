db1c3eb4bf5d0e7f068b522f751966f8c0df2a50
Numernabis <luigi.zielone@gmail.com>
2018-06-12 16:26:17 +0200

    lab5: kind of completed

--- lab5/pollution_data.ex
+++ lab5/pollution_data.ex
@@ -4,7 +4,7 @@ defmodule PollutionData do
   def importLinesFromCSV do
     list = File.read!("pollution.csv")
            |> String.split()
-    #length(list)
+    #IO.puts "wczytano: #{length(list)} linii"
   end
 
   def parseOneLine(line) do
@@ -16,6 +16,8 @@ defmodule PollutionData do
     hour = hour |> String.split(":")
            |> Enum.map(fn(x) -> elem(Integer.parse(x), 0) end)
            |> :erlang.list_to_tuple
+           |> Tuple.append(0)
+
     longitude = elem(Float.parse(longitude), 0)
     latitude = elem(Float.parse(latitude), 0)
     value = elem(Integer.parse(value), 0)
@@ -23,16 +25,23 @@ defmodule PollutionData do
   end
 
   def identifyStations do
-    map = parseLines() |> Enum.reduce(%{}, fn(d, acc) -> Map.put(acc, d.location, "xD") end)
-    #Map.size(map)
+    #map = parseLines() |> Enum.reduce(%{}, fn(d, acc) -> Map.put(acc, d.location, "xD") end)
+    list = parseLines() |> Enum.uniq_by(fn (m) -> m.location end)
+    #IO.puts "jest: #{length(list)} stacji"
   end
 
   def addStations do
-
+    :pollution_server_supervisor.start()
+    name = fn x -> "station_#{elem(x, 0)}_#{elem(x, 1)}" end
+    st = identifyStations()
+         |> Enum.map(fn (s) -> {name.(s.location), s.location} end)
+         |> Enum.each(fn {n, c} -> :pollution_gen_server.addStation(n, c) end)
   end
 
   def addValues do
-
+    vs = parseLines()
+         |> Enum.map(fn (s) -> {s.location, s.datetime, :pm10, s.pollutionLevel} end)
+         |> Enum.each(fn {n, d, t, v} -> :pollution_gen_server.addValue(n, d, t, v) end)
   end
 
   def parseLines do
@@ -41,6 +50,8 @@ defmodule PollutionData do
 
   def test do
     time_parsing = (fn() -> parseLines() end) |> :timer.tc |> elem(0)
+    time_stations = (fn() -> addStations() end) |> :timer.tc |> elem(0)
+    time_values = (fn() -> addValues() end) |> :timer.tc |> elem(0)
   end
 
 end
